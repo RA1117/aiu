656011cc637e2283bd56e68ab5cacf2ce2b520af
ArimotoYui <akasatana05280708@gmail.com>
2023-08-26 19:02:05 +0900

    シーダ

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use DateTime;
+
+
+class usersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+          
+            'name' => '名前',
+            'email' => 'メールアドレス',
+            'password' => 'パスワード',
+      
+        
+        ]);
+    }
+}
